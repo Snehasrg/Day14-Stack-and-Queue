1b42d753f8e30e14691690b3ab0ea52dff5bf7bc
Snehasrg <sghongade642@gmail.com>
2023-01-13 15:42:42 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@
             Console.WriteLine("Welcome to Data structure Problem");
 
            // PeakandPop obj = new PeakandPop();
-           CreateQueue obj = new CreateQueue(); 
+          // CreateQueue obj = new CreateQueue(); 
+            AbilityDequeue obj = new AbilityDequeue();
             Console.WriteLine("After Enqueue the elements are : ");
 
             obj.enqueue(70);
@@ -15,7 +16,8 @@
             obj.enqueue(56);
             Console.WriteLine("-------------------");
            
-            //Console.WriteLine("Item popped from Queue = " + obj.dequeue());
+            Console.WriteLine("Elements popped from Queue = " + obj.dequeue());
+            Console.WriteLine("-------------------");
             Console.WriteLine("Peek Element of the Queue is : " + obj.peek());
 
         }
